1ae239e20f73adb1a5500513f3133c59572758d0
David Michael Harper <66442656+DavidMichaelH@users.noreply.github.com>
2022-04-24 19:43:21 -0400

    Update metropolis_hastings.py

--- statistical_mechanics_models/mcmc/metropolis_hastings.py
+++ statistical_mechanics_models/mcmc/metropolis_hastings.py
@@ -4,14 +4,12 @@ import random
 
 class MetropolisHastings(ABC):
  
-          
-    #Decided to accept or reject the value
+           
     def AcceptReject(self,x_curr,x_next):
         return random.uniform(0.0, 1.0)<self.AcceptProb(x_curr,x_next)
     
     
-        
-    #Gets the next value given the current value using previous two methods. 
+         
     def NextValue(self,current_val):
         proposed_val = self.Propose(current_val)
         if self.AcceptReject(current_val,proposed_val):
