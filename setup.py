a9214c6bd822229de71043598a8d97d1c36e3f60
David Michael Harper <66442656+DavidMichaelH@users.noreply.github.com>
2022-12-08 15:44:45 -0800

    updated setup

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
-
+ 
 setup(
     name='statistical_mechanics_models',
     version='1.0.0',
+    packages=find_packages(),
     url='https://github.com/DavidMichaelH/StatisticalMechanicsModels/tree/master/statistical_mechanics_models.git',
-    author='David Michael Harper',
+    license='MIT',
     author_email='dmikeharper@gmail.com',
     description='statistical mechanics models',
+    install_requires=['numpy', 'matplotlib'],
 )
